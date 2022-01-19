395bc5f6f4498ac61b931a1ae40bcc813c01cc96
deankale <codekale@gmail.com>
2022-01-19 09:46:58 -0800

    initial portfolio

---
+++ src/data.js
@@ -0,0 +1,57 @@
+
+export const projects = [
+  {
+    title: "Music Productions",
+    subtitle: "Ableton Live 11",
+    description:
+      "I've released a full album and several singles under the pseudonym 'dean kale'. Creating music was my first passion and it's something I find myself coming back to as often as possible.",
+    image: "./music_production.gif",
+    link: "https://audius.co/deankale",
+  },
+  {
+    title: "OnTrac",
+    subtitle: "React, TailwindCSS, Project Management",
+    description:
+      "Myself and a team of 3 developers create an app for ETHGlobal's NFT Hackathon 2022. OnTrac won 1st Place on Rarible Protocol and Harmony Protocol's Prize pools, and 2nd Place on Circle's Prize pool.",
+    image: "./OnTrac.gif",
+    link: "https://showcase.ethglobal.com/nfthack2022/ontrac",
+  },
+  // {
+  //   title: " ",
+  //   subtitle: " ",
+  //   description:
+  //     " ",
+  //   image: "./ ",
+  //   link: " ",
+  // },
+];
+
+
+export const skills = [
+  "Ableton Live 11",
+  "Figma",
+  "JavaScript",
+  "React",
+  "Python",
+  "Tailwind CSS",
+  "Node",
+  "Notion",
+];
+
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
